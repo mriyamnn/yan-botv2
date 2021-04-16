09ea86bd13f5969c1cda128c20aaa4838089c3ed
mriyamnn <81896388+mriyamnn@users.noreply.github.com>
2021-04-16 13:48:28 +0700

    Add files via upload

---
+++ mriyamnn/donasi.js
@@ -0,0 +1,18 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━━━━━━
+┃            _Mau donasi gak nih?_
+┣━━━━━━━━━━━━━━━━━━━━━━━━━
+┣━━━━━‣◉ *Yummy* ◉━━━━━━
+┃▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
+┣━‣ *Gopay-Dana-Ovo*
+┣‣ 08995618868
+┣━‣ *Pulsa*
+┣‣ 08995618868
+┣━‣ *Saweria*
+┣‣ https://saweria.co/IyanGanteng
+┃▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
+┗━━━━━━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
---
+++ mriyamnn/rules.js
@@ -0,0 +1,21 @@
+const rules = (prefix) => { 
+	return `
+*rules bagi pengguna yan-bot*
+    
+‣ tolong gunakan dengan delay, jangan spam.
+‣ jangan telepon atau vc bot.
+‣ jika bot tidak aktif itu artinya rusak atau emang di matiin onwer.
+‣ patuhi aturan jauhi larangan
+
+*konsekuensi bila Melanggar rules*
+yan-bot akan memblokir kamu atau keluar dari grup yang kamu kelola.
+━━━━━━━[ *PENTING!* ]━━━━━━━━
+*‣ Iyan tidak pernah menyuruh kalian untuk donasi.
+*‣ Iyan tidak menyimpan audio,video,image,dll yang kalian add, itu hanya masuk database bot.*
+*‣ Iyan tidak pernah meminta kalian memberika informasi pribadi.*
+*‣ jika terjadi bug atau hal lainnya lapor ke onwer.*
+*‣ jika kamu menelpon yan-bot lalu di banned onwer tidak bertanggung jawab.*
+*‣ apapun yang kamu perintah kepada yan-bot, Iyan tidak bertanggung jawab sama sekali. *
+━━━━━━━━━━━━━━━━━━━━━━━━`
+}
+exports.rules = rules
