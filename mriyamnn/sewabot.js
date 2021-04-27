f8562669780735dda509e0ed1383654b8331f30b
mriyamnn <81896388+mriyamnn@users.noreply.github.com>
2021-04-27 21:52:47 +0700

    Add files via upload

---
+++ mriyamnn/sewabot.js
@@ -0,0 +1,19 @@
+const sewabot = () => { 
+	return `
+*Open sewa bot*
+
+⸨   *keuntungan*  𝒆⸩
+_menjadi admin kedua_
+_mengatur bot_
+_etc_
+
+sewa sebulan : 10k
+
+*Payment :*
+*Gopay | Dana | OvO | Three*
+
+kalo mau tinggal dm (っ'-')╮=͟͟͞͞ ✉️
+Wa.me/628995618868
+`
+}
+exports.sewabot = sewabot
